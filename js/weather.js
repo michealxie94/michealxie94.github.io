c03bc78703656e1445eef5063d731804b5633b7c
michealxie94 <michealxie94@qq.com>
2023-07-05 11:40:34 +0800

    Site updated: 2023-07-05 11:40:33

---
+++ js/weather.js
@@ -0,0 +1,23 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "5",
+    "tmpColor": "FFFFFF",
+    "tmpSize": "16",
+    "cityColor": "FFFFFF",
+    "citySize": "16",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "fixed": "true",
+    "vertical": "top",
+    "horizontal": "left",
+    "left": "150",
+    "top": "10",
+    "key": "ebd125e149fd43b6bd78a9c389fba133"
+  }
+}
